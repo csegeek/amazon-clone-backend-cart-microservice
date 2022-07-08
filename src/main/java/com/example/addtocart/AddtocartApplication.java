4a7a2d4a67bb7f9fa05f16680f766a1144072808
Vikash Kumar Singh <68193781+csegeek@users.noreply.github.com>
2022-07-08 12:34:48 +0530

    Update AddtocartApplication.java

--- src/main/java/com/example/addtocart/AddtocartApplication.java
+++ src/main/java/com/example/addtocart/AddtocartApplication.java
@@ -24,7 +24,7 @@ public class AddtocartApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST","PUT", "DELETE");
+				registry.addMapping("/**").allowedOrigins("https://vik-amazon-clone-frontend.herokuapp.com").allowedMethods("GET", "POST","PUT", "DELETE");
 				;
 			}
 		};
